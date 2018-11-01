87a9d7e8439c4c85a5ec3371fc1346e69ebdf922
mayouzi <mayormail@qq.com>
2018-11-01 17:50:04 +0800

    add 401 check

--- verdict/decorator.py
+++ verdict/decorator.py
@@ -1,6 +1,6 @@
 import functools
 
-from .exceptions import NoPermissionException
+from .exceptions import NoPermissionException, NoLoginException
 from .shortcuts import get_user_permissions_map
 from .utils import validate_permission
 
@@ -11,11 +11,12 @@ def required_permission(permission):
     def decorate(func):
         @functools.wraps(func)
         def wrapper(view, request, *args, **kwargs):
-            if hasattr(request, 'user'):
-                user_permissions = get_user_permissions_map(request.user)
-                for p in permissions:
-                    if p in user_permissions:
-                        return func(view, request, *args, **kwargs)
+            if not hasattr(request, 'user'):
+                raise NoLoginException()
+            user_permissions = get_user_permissions_map(request.user)
+            for p in permissions:
+                if p in user_permissions:
+                    return func(view, request, *args, **kwargs)
             raise NoPermissionException()
         return wrapper
     return decorate
@@ -27,11 +28,12 @@ def required_func_permission(permission):
     def decorate(func):
         @functools.wraps(func)
         def wrapper(request, *args, **kwargs):
-            if hasattr(request, 'user'):
-                user_permissions = get_user_permissions_map(request.user)
-                for p in permissions:
-                    if p in user_permissions:
-                        return func(request, *args, **kwargs)
+            if not hasattr(request, 'user'):
+                raise NoLoginException()
+            user_permissions = get_user_permissions_map(request.user)
+            for p in permissions:
+                if p in user_permissions:
+                    return func(request, *args, **kwargs)
             raise NoPermissionException()
         return wrapper
     return decorate
